b6323a74b4c4ba1eb69f5ffccd88a5bfbfa20943
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-20 18:29:17 -0300

    Revisando padrões de comentários no JavaScript

---
+++ scr/comentarios.js
@@ -0,0 +1,7 @@
+//console.log -> Escreve alguma mensagem no console devTools
+/*
+    Comentário multi linha
+    ou
+    comentário de bloco
+*/
+console.log('Bem vindo ao Starter!') //testando atalho de teclado para o VScode ........ Alt+z
--- scr/scripts.js
+++
@@ -1,1 +0,0 @@
-console.log('Bem vindo ao Starter!!')

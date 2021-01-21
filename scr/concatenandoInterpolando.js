17f9f74f661439fb34cf079f733c823f81732e08
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-21 19:47:25 -0300

    Concatenando e Interpolando variáveis

---
+++ scr/concatenandoInterpolando.js
@@ -0,0 +1,26 @@
+// Variáveis e tipo de dados
+//declaraçã or declaration
+    //let name (mas estava dando erro pedindo para ser um const)
+
+//assignment or atribuição de valores
+let name = "Ricardo"
+
+//que tipo de dado foi colocado na variável
+console.log(typeof name);
+
+// agrupamento de declarações
+let age, isHuman
+
+age = 38
+isHuman = true
+
+// multiplos argumentos na função
+console.log(name, age, isHuman)
+
+// Escrita de texto + variáveis
+
+    // concatenando valores
+    console.log('O ' + name + ' tem ' + age + ' anos')
+
+    // interpolando valores com template literals or template strings
+    console.log(`o ${name} tem ${age} anos.`)

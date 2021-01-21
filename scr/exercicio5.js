79d7885c8cda54e61f5d400af19d9735655fe86b
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-21 20:48:53 -0300

    Exercícios para memórizar conceitos revisados em JS

---
+++ scr/exercicio5.js
@@ -0,0 +1,58 @@
+// 1
+    //let weight
+
+// 2
+    //console.log(typeof weigth)
+
+// 3
+/*
+let name = "Lucas" // String
+let age = 22 // integer
+let stars = 4.5 // float
+let isSubscribed = true // boolean
+*/
+
+// 4
+const student = {
+    name: "Lucas",
+    age: 22,
+    weight: 72.8,
+    isSubscribed: true
+};
+
+// 4.1
+    //console.log(student)
+
+// 4.2
+    //console.log(`${student.name} de idade ${student.age} pesa ${student.weight}`)
+
+// 5
+let students = []
+
+// 6
+students = [
+    student
+]
+//console.log(students)
+
+// 7
+    //console.log(students[0])
+
+// 8
+const john = {
+    name: "John",
+    age: 23,
+    weight: 86.3,
+    isSubscribed: true
+}
+students [1] = john
+
+// 9
+console.log(a)
+var a = 1
+// hoisting
+
+// 10
+    //console.log(b)
+let b = 1
+    //ocorre um erro de referência 

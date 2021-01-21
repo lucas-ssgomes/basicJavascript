cba4728549b56e876fcec9fbabddb9db5e03ebeb
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-21 18:24:51 -0300

    Conceitos de váriaveis dentro do JavaScript

---
+++ scr/variaveis.js
@@ -0,0 +1,21 @@
+/**
+ * Variaveis
+
+    * Identificadores
+    * Atalhos de código
+    * 3 palavras reservadas para criar uma variável:
+        * var
+        * let
+        * const
+ */
+var clima = "Quente"
+clima = "Frio"
+console.log(clima)
+
+const estacao = "Verão"
+//estacao = "Inverno"
+console.log(estacao)
+
+let temperatura = 35
+temperatura = true
+console.log(typeof temperatura)

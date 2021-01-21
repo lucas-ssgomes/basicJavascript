b668ce62a13d7483a745f389ca8167fd9b29edc7
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-21 00:50:33 -0300

    Conceitos de String e Number

---
+++ scr/scripts.js
@@ -0,0 +1,30 @@
+/*
+   String
+
+   "" aspas duplas
+   '' aspas simples
+   `` // template literals ou template strings
+
+ */
+console.log("Lucas")
+console.log('"Lucas" "Santos"')
+console.log(`
+Lucas
+
+`)
+console.log(`Lucas ${1+1}`)
+                // Interpolação -> Capacidade de colocar valores
+console.log(1+1)
+
+/*
+    Number
+        33 // inteiros
+        12.5 // reais - float
+        NaN // Not a Number
+        Infinity // infinito
+*/
+console.log(33)
+console.log(12.5)
+console.log(45.7 / 5)
+console.log(12.5 /"asft")
+console.log(12.5 === Infinity)

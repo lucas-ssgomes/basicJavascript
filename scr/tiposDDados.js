8491a4deca26767eba4e83fe0e99ddaac5bf1153
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-21 01:00:58 -0300

    Conceitos de Boolean e undefined vs null || Movidos para arquivo tiposDDados.js

---
+++ scr/tiposDDados.js
@@ -0,0 +1,52 @@
+/*
+   String
+
+   "" aspas duplas
+   '' aspas simples
+   `` // template literals ou template strings
+
+ */
+console.log("Lucas")
+console.log('"Lucas" "Santos"')
+console.log(`
+Lucas
+
+`)
+console.log(`Lucas ${1+1}`)
+                // Interpolação -> Capacidade de colocar valores
+console.log(1+1)
+
+/*
+    Number
+        33 // inteiros
+        12.5 // reais - float
+        NaN // Not a Number
+        Infinity // infinito
+*/
+console.log(33)
+console.log(12.5)
+console.log(45.7 / 5)
+console.log(12.5 /"asft")
+console.log(12.5 === Infinity)
+
+/*
+    Boolean
+        * somente 2 valores
+
+        true // verdadeiro
+        false // falso
+*/
+console.log(true)
+console.log(false)
+
+/*
+    undefined
+        * indefinido
+    
+    null
+        * nulo
+        *objeto que possui nada dentro
+        *diferente de indefinido
+*/
+console.log(undefined)
+console.log(null)

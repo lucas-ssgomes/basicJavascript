68f11ddd357889305eb0aa861d0bee2bee80d5c5
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-21 01:04:42 -0300

    Revisando conceitos de Objetos em JavaScript

---
+++ scr/scripts.js
@@ -0,0 +1,15 @@
+/*
+    Object
+        *objeto
+        *Propriedades /Atributos
+        *Funcionalidades / Métodos
+
+    {propriedade:"valor"}
+*/
+console.log({
+    name:"Lucas",
+    idade: "22",
+    andar: function() {
+        console.log('andar')
+    }
+})

ab408e2b7bcb7a987e3343196dccf43281d1c851
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-21 20:17:43 -0300

    Update file array.js with constant array

--- scr/array.js
+++ scr/array.js
@@ -10,3 +10,24 @@ console.log([
     22,
     "sorvete"
 ])
+
+// Declaring constant Array
+
+const animals = [
+    'Lion',
+    'Monkey',
+    {
+        name: 'Cat',
+        age: 3
+    }
+]
+
+// acessar valores dentro do Array
+console.log(animals[1])
+
+// posição dentro do Array é indefined
+console.log(animals[3])
+
+// acessar valores dentro do Array sendo um Objeto
+console.log(animals[2].age)
+// Melhor padronizar sempre o tipo dos array para evitar problemas

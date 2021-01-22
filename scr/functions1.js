d28459f8d43ebf5c3e9d3b00d3af101466dc6c4a
lucas-ssgomes <lucas.lssg@gmail.com>
2021-01-21 21:23:01 -0300

    Revisando conceitos de funções

---
+++ scr/functions1.js
@@ -0,0 +1,21 @@
+/**
+ * Functions
+    * -> Agrupamento de código;
+    * -> Reutilização de código;
+    * -> Dar significado à um bloco de código;
+ */
+
+// declaration - declaração da função
+// function statement
+function createPhrases() {
+    console.log('Squirtle é o melhor inicial')
+    console.log('Digimon não é melhor que Pokemon')
+    console.log('Haikyuu é o melhor anime de esportes que já existiu!')
+}
+
+// executar a função
+// rodar, chamar, invocar
+// execute, run, call, invoke
+createPhrases()
+
+console.log('---- Fim do programa ----')
